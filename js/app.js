0e56b0995d5a4c34ae4191346fb2768d803769bc
aymen ben salah <minouaward@gmail.com>
2017-05-15 19:11:11 +0100

    try to fix issue while adding new user

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 var name = document.getElementById("name"),
     addBtn = document.getElementById("addBtn"),
-    db = openDatabase("Users", "0.1", "adding user", "1024*1024*1024");
+    db = openDatabase("Users", "0.1", "Adding user", "1024*1024*1024");
 
 db.transaction(function(transaction) {
     transaction.executeSql(
-        "CREATE TABLE if not exist user(id int primarykey," + "name text)",
+        "CREATE TABLE if not exist user(id int primarykey," 
+        + "name text)",
         []
     );
 });
+
+addBtn.onclick = function() {
+	db.transaction(function(tx) {
+        tx.executeSql(
+        	"INSERT INTO user(id , name)" + "VALUES(?,?)", 
+        	[55, name.value]
+        );
+    });
+};
