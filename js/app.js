ef9921b4ff637479ae3632ef759f2a0598ebfcea
aymen ben salah <minouaward@gmail.com>
2017-05-15 19:04:25 +0100

    test if the table exist in the sql instrument

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 var name = document.getElementById("name"),
     addBtn = document.getElementById("addBtn"),
-    db = openDatabase("addUser", "0.1", "adding user", "1024*1024*1024");
+    db = openDatabase("Users", "0.1", "adding user", "1024*1024*1024");
 
 db.transaction(function(transaction) {
     transaction.executeSql(
-        "CREATE TABLE user(id int primarykey," + "name text)",
+        "CREATE TABLE if not exist user(id int primarykey," + "name text)",
         []
     );
 });
