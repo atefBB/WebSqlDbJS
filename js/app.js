e07a37144ac1916b11897124cedee9f3677c5895
aymen ben salah <minouaward@gmail.com>
2017-05-15 19:01:44 +0100

    format

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 var name = document.getElementById("name"),
-  addBtn = document.getElementById("addBtn"),
-  db = openDatabase("addUser", "0.1", "adding user", "1024*1024*1024");
+    addBtn = document.getElementById("addBtn"),
+    db = openDatabase("addUser", "0.1", "adding user", "1024*1024*1024");
 
 db.transaction(function(transaction) {
-  transaction.executeSql("CREATE TABLE user(id int primarykey," + "name text)", []);
+    transaction.executeSql(
+        "CREATE TABLE user(id int primarykey," + "name text)",
+        []
+    );
 });
